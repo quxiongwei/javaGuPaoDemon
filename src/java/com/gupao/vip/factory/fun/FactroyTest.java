60d74e1704ed9fb25245245efba26121fc0a72ab
121768629@qq.com <121768629@qq.com>
2019-03-07 11:51:42 +0800

    Revert "工厂模式代码提交"
    This reverts commit 7c0e847

--- src/java/com/gupao/vip/factory/fun/FactroyTest.java
+++
@@ -1,9 +0,0 @@
-package java.com.gupao.vip.factory.fun;
-
-public class FactroyTest {
-    public static void main(String[] args) {
-        Factroy factroy = new GuangMingFactroy();
-        System.out.println("factroy.getMilk() = " + factroy.getMilk());
-
-    }
-}
